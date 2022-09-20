6c6e00756458d9be2944eba4de1e8d15e3dfb289
SamBawz <sambosuitstein@hotmail.com>
2022-09-20 11:22:44 +0200

    POC currently on hold since the google API requires a payment method (credit card)

---
+++ method-two.js
@@ -0,0 +1,10 @@
+let mapTwo;
+
+function initMap() {
+    mapTwo = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+}
+
+window.initMap = initMap;
